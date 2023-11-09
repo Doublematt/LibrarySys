dcc4a70693dbec19430b6b2bc0766becff1d0e3f
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2023-11-09 21:59:52 +0100

    added and tested book model

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.doublematt.librarysys/LibrarySys/models"
 )
+
+type Book models.Book
 
 func main() {
 	router := gin.Default()
@@ -17,4 +20,8 @@ func main() {
 
 func checkConnection(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "Pong"})
+}
+
+var books = []Book{
+	{Id: 1, Title: "The beginning", Author: "Dan Brown"},
 }
