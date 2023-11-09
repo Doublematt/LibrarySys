b2af3f441bcbce71b185b080844afc09543094d6
Mateusz Matusewicz <mateusz.matusewicz16@gmail.com>
2023-11-09 23:19:33 +0100

    added some comments

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ import (
 	"github.doublematt.librarysys/LibrarySys/models"
 )
 
+// init models
 type Book models.Book
 
 func main() {
 	router := gin.Default()
 
+	// all endpoints available
 	// check connection
 	router.GET("/ping", checkConnection)
 	router.GET("/book/:id", getBookBtId)
@@ -20,12 +22,17 @@ func main() {
 	router.Run()
 }
 
+//
+// CRUD methods
+//
+
 func checkConnection(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, gin.H{"message": "Pong"})
 }
 
 func getBookBtId(c *gin.Context) {
 
+	// converting string param to int value
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 		panic(err)
@@ -35,9 +42,15 @@ func getBookBtId(c *gin.Context) {
 
 }
 
+// temporary db for development purpose
+
 var books = []Book{
 	{Id: 1, Title: "The beginning", Author: "Dan Brown"},
 }
+
+//
+// secondary methods
+//
 
 func findBook(id int) Book {
 
